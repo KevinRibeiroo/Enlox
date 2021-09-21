ff9c6e80b6874adf025b51814d353642c5c55013
Dani <daniellyccn@gmail.com>
2021-09-21 08:28:15 -0300

    ok

---
+++ reactjs/src/pages/home/index.js
@@ -0,0 +1,61 @@
+export default function Home(){
+    return(
+        <hContainer>
+            <div className = "hSubcabecalho">
+                <div className = "hCategorias">
+                    <div className = "hEletronicos">
+                        <div></div>
+                        <div>ELETRÔNICOS</div>
+                    </div>
+
+                    <div className = "hMobilia">
+                        <div></div>
+                        <div>MOBÍLIA</div>
+                    </div>
+
+                    <div className = "hBeleza">
+                        <div></div>
+                        <div>BELEZA</div>
+                    </div>
+
+                    <div className = "hEsportes">
+                        <div></div>
+                        <div>ESPORTES</div>
+                    </div>
+
+                    <div className = "hConstrucao">
+                        <div></div>
+                        <div>CONSTRUÇÃO</div>
+                    </div>
+
+                    <div className = "hBrinquedos">
+                        <div></div>
+                        <div>BRINQUEDOS</div>
+                    </div>
+
+                    <div className = "hAutopecas">
+                        <div></div>
+                        <div>AUTO-PEÇAS</div>
+                    </div>
+
+                    <div className = "hOutros">
+                        <div></div>
+                        <div>OUTROS</div>
+                    </div>
+                    
+                </div>
+                <div className = "hPesquisar">
+                    <div className = "hProcurando"></div>
+                    <div className = "hSimbolo">
+                        <div className = "hLupa"></div>
+                        <div className = "hPesquisar">Pesquisar</div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+        </hContainer>
+    )
+}
