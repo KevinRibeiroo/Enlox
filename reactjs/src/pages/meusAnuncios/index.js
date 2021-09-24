ff1e474f7abaf1cdf4e64f2bab40d5fe96c929aa
ricky-d-c <you@example.com>
2021-09-24 19:38:55 -0300

    ok

--- reactjs/src/pages/meusAnuncios/index.js
+++ reactjs/src/pages/meusAnuncios/index.js
@@ -19,9 +19,10 @@ export default function MeusAnuncios(){
                      <div className="comoFunciona"> <a href="">Como funciona?</a></div> 
                   </div>
 
-                  
+                 <div className="butoes"> 
                   <button className="editar"> Editar </button>
                   <button className="excluir"> Excluir </button>
+                 </div>    
           </div>
             
             <hr/>
@@ -37,9 +38,10 @@ export default function MeusAnuncios(){
                      <div className="comoFunciona"> <a href="">Como funciona?</a></div> 
                   </div>
 
-                  
+                 <div className="butoes"> 
                   <button className="editar"> Editar </button>
                   <button className="excluir"> Excluir </button>
+                 </div>    
           </div>
 
           <hr/>
@@ -54,11 +56,31 @@ export default function MeusAnuncios(){
                      <div className="comoFunciona"> <a href="">Como funciona?</a></div> 
                   </div>
 
-                  
+                  <div className="butoes"> 
                   <button className="editar"> Editar </button>
                   <button className="excluir"> Excluir </button>
+                 </div>    
           </div>
           
+          <hr/>
+
+          <div className="agp">
+              <div className="img-agp"><img src="/assets/images/microondas.jpg" alt="" /></div>
+              <div className="texto1"> 
+                  <div className="nm_produto">Micro-ondas</div>
+                  <div className="preço"> preço: R$350.00</div>
+                  </div>
+                  <div className="premium">
+                     <div className="texto">Destacar anuncio</div>
+                     <div className="comoFunciona"> <a href="">Como funciona?</a></div> 
+                  </div>
+
+                 <div className="butoes"> 
+                  <button className="editar"> Editar </button>
+                  <button className="excluir"> Excluir </button>
+                 </div>    
+          </div>
+
             <Rodape />
      </Container>
      )}
--- reactjs/src/pages/meusAnuncios/styled.js
+++ reactjs/src/pages/meusAnuncios/styled.js
@@ -5,17 +5,34 @@ const Container = styled.div`
 .agp{
     display: flex;
     flex-direction: row;
+    margin: 40px ;
 }
 
-.img-agp{
-  
+.img-agp img{
+    max-width: 200px;
+    border: solid;
+    border-width: 2.4px;
+    border-radius: 10px;
 }
 
 .texto1{
     display: flex;
     flex-direction: column;
+    margin: 3em 5em 0px 1em ;
 }
 
-
+.premium{
+    margin-left: 7em;
+    margin-top: 3em;
+}
+.butoes{
+    display: flex;
+    flex-direction: column;
+    margin-left: 17em;
+    margin-top: 3em;
+}
+.editar{
+   
+}
 `
 export default Container 
