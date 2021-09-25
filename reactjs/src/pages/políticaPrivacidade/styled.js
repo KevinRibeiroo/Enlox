f0a26ff434d6a85cc0ecfe5706e0cce19d126e10
Danielly <daniellyccn@gmail.com>
2021-09-25 15:18:52 -0300

    ok

--- reactjs/src/pages/políticaPrivacidade/styled.js
+++ reactjs/src/pages/políticaPrivacidade/styled.js
@@ -3,16 +3,22 @@ import styled from "styled-components";
  
  
  const PContainer = styled.div`
- 
+        display: flex;
+        flex-direction: column;
     .PConteudo{
-        padding: 3em 10em;
+        padding: 3em;
         text-align: left;
         line-height: 2em;
         font: 1.5em Roboto-Slab;
+        width: 70%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-self: center;
         .PTitulo{
             text-align:center;
             margin-bottom: 2em;
-            font: 2em;
+            font: 2em Stoke-Regular;
         }
     }
  `
