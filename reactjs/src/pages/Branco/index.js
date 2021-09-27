6cf43f029f586f1668e1525bae4d47ec7548c07c
ricky-d-c <you@example.com>
2021-09-27 08:47:32 -0300

    ok

--- reactjs/src/pages/Branco/index.js
+++ reactjs/src/pages/Branco/index.js
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom"
+
 export default function Index(){
     return(
         <div className = "Container">
-            oieeeeeeeee
+           <Link to="/home"><b>Home   │</b></Link>
+           <Link to="/"><b>carrinho   │</b></Link>
+           <Link to="/termosServiço"><b>Tela termosServiço   │</b></Link>
+           <Link to="/sobreNos"><b>Tela Sobre nos   │</b></Link>
+           <Link to="/meusAnuncios"><b>Tela Meus Anuncios   │</b></Link>
+           <Link to="/produto"><b>Tela Produto   │</b></Link>
+           <Link to="/politicaDePrivacidade"><b>Tela politica De Privacidade  │</b></Link>
+           <Link to="/userRegistrado"><b>Tela userRegistrado   │</b></Link>
+           <Link to="/perfil"><b>Tela perfil   │</b></Link>
+           <Link to="/editarProduto"><b>Tela Editar Produto   │</b></Link>
         </div>
     )
 }
