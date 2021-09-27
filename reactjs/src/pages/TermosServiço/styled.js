c106d4f57a3404d3e9a15b25be9ed6e459aa32ed
ricky-d-c <you@example.com>
2021-09-27 08:56:13 -0300

    ok

--- reactjs/src/pages/TermosServiço/styled.js
+++ reactjs/src/pages/TermosServiço/styled.js
@@ -1,16 +1,33 @@
 import styled from 'styled-components'
 
 const tsContainer = styled.div`
- 
-.titulo{
-    text-align: center;
-}
 
+display: flex;
+flex-direction: column;
 
+.tsTermos{
+  padding: 3em;
+  text-align: left;
+  line-height: 2em;
+  font: 1em Roboto-Slab;
+  width: 70%;
+  
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-self: center;
 
-.tsTermos {
   text-align: center;
-  font-size: 17px;
+  font: 1em Roboto-Slab;
+.titulo{
+    text-align:center;
+    margin-bottom: 2em;
+    font: 2em Stoke-Regular;
 }
+
+}
+ 
+
+
 `
 export default tsContainer 
