5adf67fb774ed1f091cf9bff3c4e00cfea99e8b5
ricky-d-c <you@example.com>
2021-10-01 11:36:57 -0300

    ok

--- reactjs/src/pages/editarProduto/index.js
+++ reactjs/src/pages/editarProduto/index.js
@@ -10,8 +10,27 @@ export default function Home(){
              <button className="btn-salvar">salvar</button>
              <div className="box">
                  <div className="agp1">
-                     <div className="titulo">Geladeira Frigobar</div>
-                     <div className="btn-editar"><img src="../../assets/images/editar.svg"/></div>
+                        <div className="product">
+                         <div className="titulo">
+                            <div className="title"><h2>Geladeira Frigobar</h2></div>
+                            <div className="btn-editar"><img alt="" src="/assets/images/editar.svg"/></div>
+                         </div>
+                            <div className="imgs-product">
+                                <div className="img-principal"><img src="/assets/images/geladeira.svg" alt=""/></div>
+                                <div className="seta"><img src="/assets/images/Seta.png" alt="" /></div>
+                                <div className="agp-product">
+                                <div className="product-min"><img  alt="" src="/assets/images/geladeira.svg" /></div>
+                                <div className="product-min"><img  alt="" src="/assets/images/geladeira.svg" /></div>
+                                <div className="product-min"><img  alt="" src="/assets/images/geladeira.svg" /></div>
+                                <div className="product-min"><img  alt="" src="/assets/images/geladeira.svg" /></div>
+                            </div>
+                            <div className="preco"><div className="title-preco"> Preço: </div> <span>R$ 1381.09</span> </div>
+                            <div>
+                                <div className="desc-produt">Geledeira frigobar 2020, usada e em otimo estado</div>
+                                <div ><button className="mais">Mostrar mais </button></div>
+                            </div>
+                        </div> 
+                     </div>
                  </div>
              </div>
             <Rodape />
--- reactjs/src/pages/editarProduto/styled.js
+++ reactjs/src/pages/editarProduto/styled.js
@@ -3,6 +3,14 @@ import styled from 'styled-components';
 
 const Container = styled.div`
 
+display: flex;
+flex-direction: column;
+
+.titulo{
+    display: flex;
+    flex-direction: row;
+}
+
 .box{
     background: rgba(188, 191, 80, 0.42) ; 
 }
@@ -12,6 +20,19 @@ const Container = styled.div`
     
 }
 
+.img-principal img{
+    width: 13em;
+    height: auto;
+}
+
+.product-min img{
+    width: 3em;
+    height: auto;
+}
+
+.mais{
+    border: hidden; 
+}
 `
 
 export{Container};
