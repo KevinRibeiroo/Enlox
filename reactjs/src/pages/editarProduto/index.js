6902b6dd4d4a3427549ca95842e88a3eba7b9a5f
ricky-d-c <you@example.com>
2021-10-02 18:23:27 -0300

    ok

--- reactjs/src/pages/editarProduto/index.js
+++ reactjs/src/pages/editarProduto/index.js
@@ -1,39 +1,42 @@
-import Cabecalho from "../../components/cabecalho";
-import Rodape from "../../components/rodape";
+
+import Cabecalho from "../../components/cabecalho"
+import Rodape from "../../components/rodape"
 
 import {Container} from './styled.js';
 
 export default function Home(){
     return(
         <Container>
+          <main>
             <Cabecalho/>
              <button className="btn-salvar">salvar</button>
-             <div className="box">
-                 <div className="agp1">
-                        <div className="product">
-                         <div className="titulo">
-                            <div className="title"><h2>Geladeira Frigobar</h2></div>
-                            <div className="btn-editar"><img alt="" src="/assets/images/editar.svg"/></div>
-                         </div>
-                            <div className="imgs-product">
-                                <div className="img-principal"><img src="/assets/images/geladeira.svg" alt=""/></div>
-                                <div className="seta"><img src="/assets/images/Seta.png" alt="" /></div>
-                                <div className="agp-product">
-                                <div className="product-min"><img  alt="" src="/assets/images/geladeira.svg" /></div>
-                                <div className="product-min"><img  alt="" src="/assets/images/geladeira.svg" /></div>
-                                <div className="product-min"><img  alt="" src="/assets/images/geladeira.svg" /></div>
-                                <div className="product-min"><img  alt="" src="/assets/images/geladeira.svg" /></div>
+             <div className="conteudo">
+                    <div className="group">
+                        <div className="agp1">
+                            <div className="agp-texto">
+                                <div className="title"><h2>Geladeira Frigobar</h2></div>
+                                <div className="editar"><img alt="" src="/assets/images/editar.svg"/></div>
                             </div>
-                            <div className="preco"><div className="title-preco"> Preço: </div> <span>R$ 1381.09</span> </div>
+                               <div className="agp-img">
+                                  <div className="img-principal"><img alt="" src="/assets/images/geladeira.svg"/></div>
+                                   <div className="seta"><img alt="" src="/assets/images/seta.png"/></div>
+                                     <div className="agp-min">   
+                                        <div className="little-img"><img alt="" src="/assets/images/geladeira.svg"/></div>
+                                        <div className="little-img"><img alt="" src="/assets/images/geladeira.svg"/></div>
+                                        <div className="little-img"><img alt="" src="/assets/images/geladeira.svg"/></div>
+                                        <div className="little-img"><img alt="" src="/assets/images/geladeira.svg"/></div>
+                                     </div>   
+                               </div>
+                            <div className="preco"><div className="title-preco"> Preço: </div> <span>R$ 1100.09</span> </div>
                             <div>
                                 <div className="desc-produt">Geledeira frigobar 2020, usada e em otimo estado</div>
-                                <div ><button className="mais">Mostrar mais </button></div>
+                                <div >  <button className="mais">Mostrar mais </button></div>
                             </div>
-                        </div> 
-                     </div>
-                 </div>
-             </div>
+                        </div>
+                    </div>
+                </div>
             <Rodape />
+            </main>
         </Container>
     )
 }
--- reactjs/src/pages/editarProduto/styled.js
+++ reactjs/src/pages/editarProduto/styled.js
@@ -6,33 +6,138 @@ const Container = styled.div`
 display: flex;
 flex-direction: column;
 
-.titulo{
+main {
+    display: flex;
+    flex-direction: column;
+
+
+    height: 120vh;
+    justify-content: space-between;
+}
+
+.conteudo {
+    display: flex;
+    flex-direction: column;
+    
+    align-self: center;
+    width: 75vw;
+    background-color: rgba(188, 191, 80, 0.6);
+    
+    height: 85vh;
+    justify-content: center;
+}
+
+.group {
+    display: flex;
+    background: #ECF6FE;
+    border: 1px solid #4F6E92;
+    box-sizing: border-box;
+    width: 90%;
+    height: 90%;
+
+    align-self: center;
+    justify-content: space-around;
+    align-items: center;
+}
+
+.agp1 {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+   
+}
+
+.agp-texto{
     display: flex;
     flex-direction: row;
 }
 
-.box{
-    background: rgba(188, 191, 80, 0.42) ; 
+.title {
+    font-family: Roboto Slab-Bold;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 1.3em;
+    line-height: 47px;
+
+    color: #353535;
 }
 
-.agp1{
-    background-color: #ffffff;
-    
+.agp-img{
+    display: flex;
+    flex-direction: row;
 }
 
-.img-principal img{
+.agp-min{
+    display: flex;
+    flex-direction: column;
+}
+
+.preco {
+    display: flex;
+    font-family: Roboto-Slab;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 1.4em;
+   
+
+    color: #353535;
+    align-items: center;
+    justify-content: space-between;
+    width: 9.5em;
+
+    height: 2.5em;
+}
+
+.preco > span {
+    padding-top: 0.3em;
+}
+.title-preco  {
+    font-family: Roboto Slab-Bold;
+    font-style: normal;
+    font-weight: bolder;
+    font-size: 1.4em;
+ 
+
+    color: #353535;
+}
+
+.imgs-product {
+    display: flex;
+   
+    justify-content: space-evenly;
+    align-items: center;
+}
+
+.img-principal{
     width: 13em;
-    height: auto;
+     height: auto;
 }
 
-.product-min img{
+.little-img img{
     width: 3em;
-    height: auto;
+     height: auto;
 }
 
-.mais{
-    border: hidden; 
+.mais {
+    font-family: Roboto Slab-Light;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 0.9em;
+    line-height: 26px;
+    border: hidden;
+
+    color: #545353;
+    background-color: transparent;
 }
+
+.img-principal{
+    display: flex;
+    //height: 40vh;
+    flex-direction: column;
+    justify-content: center;
+}
+
+
 `
 
 export{Container};
