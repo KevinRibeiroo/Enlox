6e71fb6104bee43abb941b82d89fc88eca234539
ricky-d-c <you@example.com>
2021-10-20 20:49:02 -0300

    ok felas

--- reactjs/src/pages/meusAnuncios/index.js
+++ reactjs/src/pages/meusAnuncios/index.js
@@ -4,72 +4,38 @@ import Cabecalho from "../../components/cabecalho"
 import Rodape from "../../components/rodape"
 import Container from './styled'
 
+import Api from '../../service/api';
+import { useState, React, useEffect } from 'react';
+const api = new Api();
+
 
 export default function MeusAnuncios(){
+
+    const [produto, setProduto] = useState([]);
+   
+    async function listar(){
+
+        let r = await api.listarProduto();
+        console.log(r);
+        setProduto(r);
+
+    }
+
+    useEffect(() => {
+        listar();
+    },   [])
+
+
     return(
         <Container>
             <Cabecalho />
-            <div className="agp">
-              <div className="img-agp"><img src="/assets/images/microondas.jpg" alt="" /></div>
-              <div className="texto1"> 
-                  <div className="nm_produto">Micro-ondas</div>
-                  <div className="preço"> preço: R$350.00</div>
-                  </div>
-                  <div className="premium">
-                     <div className="texto"><b>Destacar anuncio</b></div>
-                     <div className="comoFunciona"> <Link to="/meusAnuncios"><b>Como funciona?</b></Link></div> 
-                  </div>
 
-                 <div className="butoes"> 
-                  <button className="editar"> Editar </button>
-                  <button className="excluir"> Excluir </button>
-                 </div>    
-          </div>
-            
-            <hr/>
-
-            <div className="agp">
-              <div className="img-agp"><img src="/assets/images/microondas.jpg" alt="" /></div>
-              <div className="texto1"> 
-                  <div className="nm_produto">Micro-ondas</div>
-                  <div className="preço"> preço: R$350.00</div>
-                  </div>
-                  <div className="premium">
-                     <div className="texto"><b>Destacar anuncio</b></div>
-                     <div className="comoFunciona"> <Link to="/meusAnuncios"><b>Como funciona?</b></Link></div> 
-                  </div>
-
-                 <div className="butoes"> 
-                  <button className="editar"> Editar </button>
-                  <button className="excluir"> Excluir </button>
-                 </div>    
-          </div>
-
-          <hr/>
+       {produto.map((item) => 
           <div className="agp">
-              <div className="img-agp"><img src="/assets/images/microondas.jpg" alt="" /></div>
+              <div className="img-agp"><img src={item.ds_imagem}/></div>
               <div className="texto1"> 
-                  <div className="nm_produto">Micro-ondas</div>
-                  <div className="preço"> preço: R$350.00</div>
-                  </div>
-                  <div className="premium">
-                     <div className="texto"><b>Destacar anuncio</b></div>
-                     <div className="comoFunciona"> <Link to="/meusAnuncios"><b>Como funciona?</b></Link></div> 
-                  </div>
-
-                  <div className="butoes"> 
-                  <button className="editar"> Editar </button>
-                  <button className="excluir"> Excluir </button>
-                 </div>    
-          </div>
-          
-          <hr/>
-
-          <div className="agp">
-              <div className="img-agp"><img src="/assets/images/microondas.jpg" alt="" /></div>
-              <div className="texto1"> 
-                  <div className="nm_produto">Micro-ondas</div>
-                  <div className="preço"> preço: R$350.00</div>
+                  <div className="nm_produto">{item.nm_produto}</div>
+                  <div className="preço"> {item.vl_preco}</div>
                   </div>
                   <div className="premium">
                      <div className="texto"><b>Destacar anuncio</b></div>
@@ -77,10 +43,11 @@ export default function MeusAnuncios(){
                   </div>
 
                  <div className="butoes"> 
-                  <button className="editar"> Editar </button>
-                  <button className="excluir"> Excluir </button>
+                  <button /*onClick={ () => editar() }*/className="editar"> Editar </button>
+                  <button /*onClick={ () => remover() }*/ className="excluir"> Excluir </button>
                  </div>    
           </div>
+        )}
 
             <Rodape />
      </Container>
