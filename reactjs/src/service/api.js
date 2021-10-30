9c665442bac06963593f7ddadb7e2e9e1cd7b246
rickyszx <rickydiascosta13@gmail.com>
2021-10-30 17:43:13 -0300

    ok

--- reactjs/src/service/api.js
+++ reactjs/src/service/api.js
@@ -53,7 +53,7 @@ export default class Api {
                 return r.data;
             }
     
-            async alterar( id, nome, categoria, preco,  descProduto, estoque, img,) {
+            async editar( id, nome, categoria, preco,  descProduto, estoque, img,) {
                 let r = await api.put(`/produto/${id}`, { nome, categoria, preco, descProduto, estoque, img, })
                 return r.data
             } 
