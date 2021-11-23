0dbca894421c7980f8a1eb63f872e9ab9c506005
Danielly <daniellyccn@gmail.com>
2021-11-22 22:45:46 -0300

    ok

--- reactjs/src/pages/resetar/styled.js
+++ reactjs/src/pages/resetar/styled.js
@@ -5,6 +5,7 @@ height: 100vh;
 background-color:rgb(188,191,80,60%);
 display: flex;
 flex-direction: column;
+.oie{display:flex;flex-direction:row;justify-content:center;align-items:center;}
 .RBox{height:70%; width: 40%; background-color:#fff;
 display: flex;
 flex-direction: column;
@@ -13,7 +14,7 @@ align-self: center;
 margin:4em 0em;
 filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
 padding: 3em;
-
+margin-right: 2em;
 .imagem{display:flex; flex-direction:column; justify-self:center;align-self:center;margin-bottom:2em;cursor: pointer;}
 .RTitulo{
     text-align:center;
